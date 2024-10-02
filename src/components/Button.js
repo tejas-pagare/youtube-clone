9c5ad7ada901d65216a9bac314ed49e9d63e1d0b
Tejas pagare <tejaspagare1625@gmail.com>
2024-10-02 19:44:25 +0530

    small updates

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Button = ({title}) => {
+  const navigate = useNavigate();
   return (
-    <button className='bg-gray-100 hover:bg-gray-200 p-2 px-3 m-2 w-auto rounded-md text-sm font-semibold'>
+    <button onClick={navigate("/")} className='bg-gray-100 hover:bg-gray-200 p-2 px-3 m-2 w-auto rounded-md text-sm font-semibold'>
       {title}
     </button>
   )
